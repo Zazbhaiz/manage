744fbf48032bd76e4fd4a03b4e5850556fec3d10
Zazbhai <96649732+Zazbhaiz@users.noreply.github.com>
2022-12-09 15:43:25 +0530

    Update config.py

--- KRISTY/config.py
+++ KRISTY/config.py
@@ -12,7 +12,7 @@ def get_user_list(config, key):
 class Config(object):
     LOGGER = True
     MONGO_DB_URI = "mongodb+srv://vcbot:vcbot@cluster0.yqipgxg.mongodb.net/?retryWrites=true&w=majority"
-    ARQ_API_URL = "EMJCKT-LFEXOP-UVMOXX-QOMCCD-ARQ"
+    ARQ_API_KEY = "EMJCKT-LFEXOP-UVMOXX-QOMCCD-ARQ"
     # REQUIRED
     # Login to https://my.t = ""elegram.org and fill in these slots with the details given by it
     ALLOW_CHATS = True
